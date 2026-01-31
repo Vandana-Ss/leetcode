ffaa11e442692b2ab7c79aefc00084b7348a184d
Vandana Sree Sattaru <101133391+Vandana-Ss@users.noreply.github.com>
2026-01-31 18:59:09 +0530

    Refactor findMaxConsecutiveOnes method

--- Max-consecutive ones/solution.java
+++ Max-consecutive ones/solution.java
@@ -1,18 +1,18 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int c = 1, max = 0;
+        
+        int sum = 0;
+        int max = 0;
+        
         for(int i=0 ; i<nums.length; i++){
             if(nums[i]==1){
-                c+=1;
-                if(c>max){
-                    max=c;
-                }
+                sum+=1;
+                max = Math.max(sum, max);
             }
             else{
-                c=0;
+                sum=0;
             }
         }
         return max;
     }
 }
-
